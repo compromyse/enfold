f9974e06b7653f12201a258aba2078b8b76218ab
Raghuram Subramani <raghus2247@gmail.com>
2025-03-27 23:37:05 +0530

    add create_csv

---
+++ scrape_ecourtindia_v6/create_csv.py
@@ -0,0 +1,15 @@
+from tinydb import TinyDB
+import csv
+
+db = TinyDB('orders.json')
+entries = db.all()
+
+csvfile = open('orders.csv', 'w', newline='')
+w = csv.writer(csvfile)
+w.writerow(['Court Name', 'Case Info', 'Petitioner/Respondent', 'Date', 'File'])
+
+for entry in entries:
+    ent = [entry['court_name'], entry['case_info'], entry['petitioner_respondent'], entry['date'], f'http://aarch.compromyse.xyz:8000/{entry["filename"]}']
+    w.writerow(ent)
+
+csvfile.close()
